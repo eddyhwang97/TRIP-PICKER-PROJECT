1cebaaa27c914e5cab620b388b8169ecbfe01b04
eddyhwang97 <hwangdw1997@gmail.com>
2025-04-21 23:08:43 +0900

    메인 업데이트

--- my-app/src/layouts/HeaderArea.jsx
+++ my-app/src/layouts/HeaderArea.jsx
@@ -1,34 +1,29 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import smallLogo from '../assets/logo/small-logo.png';
+import React from "react";
+import { Link } from "react-router-dom";
+import smallLogo from "../assets/logo/small-logo.png";
+
+import "./css/headerArea.scss";
 
 function HeaderArea(props) {
-    const style ={
-        display: 'flex',
-        fontSize: '20px',
-        justifyContent: 'space-between',
-        alignItems: 'center',
-        padding: '10px',
-        backgroundColor: '#f8f9fa',
-        borderBottom: '1px solid #dee2e6'
-    }
-    return (
-        <>
-        <div className='header'>
-            <section className='logo-box'>
-                <img src={smallLogo}/>
-            </section>
-        </div>
-
-            <div style={style}>
-                <Link to="/">main</Link>
-                <Link to="editTrip">editTrip</Link>
-                <Link to="dashboard">dashboard</Link>
-                <Link to="login">login</Link>
-                <Link to="join">join</Link>
-            </div>
-        </>
-    );
+  
+  return (
+    <>
+      <div className="header">
+        <section className="logo-box">
+          <Link to={"/"}>
+            <img src={smallLogo} />
+          </Link>
+        </section>
+        <section className="nav-box" >
+          <Link to="/">main</Link>
+          <Link to="editTrip">editTrip</Link>
+          <Link to="dashboard">dashboard</Link>
+          <Link to="login">login</Link>
+          <Link to="join">join</Link>
+        </section>
+      </div>
+    </>
+  );
 }
 
 export default HeaderArea;
--- my-app/src/views/Main.jsx
+++ my-app/src/views/Main.jsx
@@ -1,9 +1,28 @@
 import React from "react";
-import { Outlet } from "react-router-dom";
+
+import "./css/main.scss";
+import picker from "../assets/logo/picker.png";
 
 function Main(props) {
   return (
-    <div>첫화면</div>
+    <div className="container">
+      <div className="introduction">
+        <div>
+          <p>자유로운 여행을 하고싶어?</p>
+        </div>
+        <div>
+          <p>시작해봐</p>
+          <span className="logo">
+            TRIP
+            <img src={picker} alt="picker" />
+            PICKER
+          </span>
+        </div>
+      </div>
+      <div className="start-button">
+        <button className="button">시작하기</button>
+      </div>
+    </div>
   );
 }
 
