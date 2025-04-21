5cf21b304b93e96befe6e14c9774a5264aee9586
ToffeeSta <Wjdgnsrh@gmail.com>
2025-04-21 16:40:04 +0900

    리셋,더미데이터

---
+++ my-app/src/contants/users.js
@@ -0,0 +1,10 @@
+export const users = [
+    {
+      id: "user001",
+      username: "test",
+      email: "testuser@gmail.com",
+      password: "test_pw",
+      trips: ["trip001"]
+    }
+  ];
+  
