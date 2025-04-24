a2cea62a29063b76c9d6ca808b465371b58f5e2b
ToffeeSta <Wjdgnsrh@gmail.com>
2025-04-24 15:17:54 +0900

    로컬데이터 입력

--- my-app/src/contants/citys.js
+++ my-app/src/contants/citys.js
@@ -1,104 +1,121 @@
-export const citys = {
-    seoul: {
-      name: "서울",
-      englishName: "Seoul",
-      center: { lat: 37.5665, lng: 126.9780 },
-      radiusKm: 20
-    },
-    busan: {
-      name: "부산",
-      englishName: "Busan",
-      center: { lat: 35.1796, lng: 129.0756 },
-      radiusKm: 20
-    },
-    gwangju: {
-      name: "광주",
-      englishName: "Gwangju",
-      center: { lat: 35.1595, lng: 126.8526 },
-      radiusKm: 15
-    },
-    daegu: {
-      name: "대구",
-      englishName: "Daegu",
-      center: { lat: 35.8722, lng: 128.6025 },
-      radiusKm: 15
-    },
-    jeju: {
-      name: "제주",
-      englishName: "Jeju",
-      center: { lat: 33.4996, lng: 126.5312 },
-      radiusKm: 20
-    },
-    daejeon: {
-      name: "대전",
-      englishName: "Daejeon",
-      center: { lat: 36.3504, lng: 127.3845 },
-      radiusKm: 15
-    },
-    gangneung: {
-      name: "강릉",
-      englishName: "Gangneung",
-      center: { lat: 37.7519, lng: 128.8761 },
-      radiusKm: 15
-    },
-    ulsan: {
-      name: "울산",
-      englishName: "Ulsan",
-      center: { lat: 35.5384, lng: 129.3114 },
-      radiusKm: 15
-    },
-    sokcho: {
-      name: "속초",
-      englishName: "Sokcho",
-      center: { lat: 38.2070, lng: 128.5912 },
-      radiusKm: 10
-    },
-    yeosu: {
-      name: "여수",
-      englishName: "Yeosu",
-      center: { lat: 34.7604, lng: 127.6622 },
-      radiusKm: 15
-    },
-    incheon: {
-      name: "인천",
-      englishName: "Incheon",
-      center: { lat: 37.4563, lng: 126.7052 },
-      radiusKm: 20
-    },
-    ulleungdo: {
-      name: "울릉도",
-      englishName: "Ulleungdo",
-      center: { lat: 37.4842, lng: 130.9050 },
-      radiusKm: 10
-    },
-    gyeongju: {
-      name: "경주",
-      englishName: "Gyeongju",
-      center: { lat: 35.8562, lng: 129.2247 },
-      radiusKm: 15
-    },
-    pohang: {
-      name: "포항",
-      englishName: "Pohang",
-      center: { lat: 36.0190, lng: 129.3435 },
-      radiusKm: 15
-    },
-    jeonju: {
-      name: "전주",
-      englishName: "Jeonju",
-      center: { lat: 35.8242, lng: 127.1479 },
-      radiusKm: 15
-    },
-    mokpo: {
-      name: "목포",
-      englishName: "Mokpo",
-      center: { lat: 34.8118, lng: 126.3922 },
-      radiusKm: 15
-    },
-    chungju: {
-      name: "충주",
-      englishName: "Chungju",
-      center: { lat: 36.9910, lng: 127.9260 },
-      radiusKm: 15
-    }
-  };
+export const citys = [
+  {
+    id: "seoul",
+    name: "서울",
+    englishName: "Seoul",
+    center: { lat: 37.5665, lng: 126.9780 },
+    radiusKm: 20,
+  },
+  {
+    id: "busan",
+    name: "부산",
+    englishName: "Busan",
+    center: { lat: 35.1796, lng: 129.0756 },
+    radiusKm: 20,
+  },
+  {
+    id: "gwangju",
+    name: "광주",
+    englishName: "Gwangju",
+    center: { lat: 35.1595, lng: 126.8526 },
+    radiusKm: 15,
+  },
+  {
+    id: "daegu",
+    name: "대구",
+    englishName: "Daegu",
+    center: { lat: 35.8722, lng: 128.6025 },
+    radiusKm: 15,
+  },
+  {
+    id: "jeju",
+    name: "제주",
+    englishName: "Jeju",
+    center: { lat: 33.4996, lng: 126.5312 },
+    radiusKm: 20,
+  },
+  {
+    id: "daejeon",
+    name: "대전",
+    englishName: "Daejeon",
+    center: { lat: 36.3504, lng: 127.3845 },
+    radiusKm: 15,
+  },
+  {
+    id: "gangneung",
+    name: "강릉",
+    englishName: "Gangneung",
+    center: { lat: 37.7519, lng: 128.8761 },
+    radiusKm: 15,
+  },
+  {
+    id: "ulsan",
+    name: "울산",
+    englishName: "Ulsan",
+    center: { lat: 35.5384, lng: 129.3114 },
+    radiusKm: 15,
+  },
+  {
+    id: "sokcho",
+    name: "속초",
+    englishName: "Sokcho",
+    center: { lat: 38.2070, lng: 128.5912 },
+    radiusKm: 10,
+  },
+  {
+    id: "yeosu",
+    name: "여수",
+    englishName: "Yeosu",
+    center: { lat: 34.7604, lng: 127.6622 },
+    radiusKm: 15,
+  },
+  {
+    id: "incheon",
+    name: "인천",
+    englishName: "Incheon",
+    center: { lat: 37.4563, lng: 126.7052 },
+    radiusKm: 20,
+  },
+  {
+    id: "ulleungdo",
+    name: "울릉도",
+    englishName: "Ulleungdo",
+    center: { lat: 37.4842, lng: 130.9050 },
+    radiusKm: 10,
+  },
+  {
+    id: "gyeongju",
+    name: "경주",
+    englishName: "Gyeongju",
+    center: { lat: 35.8562, lng: 129.2247 },
+    radiusKm: 15,
+  },
+  {
+    id: "pohang",
+    name: "포항",
+    englishName: "Pohang",
+    center: { lat: 36.0190, lng: 129.3435 },
+    radiusKm: 15,
+  },
+  {
+    id: "jeonju",
+    name: "전주",
+    englishName: "Jeonju",
+    center: { lat: 35.8242, lng: 127.1479 },
+    radiusKm: 15,
+  },
+  {
+    id: "mokpo",
+    name: "목포",
+    englishName: "Mokpo",
+    center: { lat: 34.8118, lng: 126.3922 },
+    radiusKm: 15,
+  },
+  {
+    id: "chungju",
+    name: "충주",
+    englishName: "Chungju",
+    center: { lat: 36.9910, lng: 127.9260 },
+    radiusKm: 15,
+  },
+];
--- my-app/src/contants/index.js
+++ my-app/src/contants/index.js
@@ -3,4 +3,19 @@ import { places } from "./places";
 import { trips } from "./trips";
 import { users } from "./users";
 
-export { citys, places, trips, users };
+const initAllData = () => {
+  if (localStorage.getItem("citys") === null) {
+    localStorage.setItem("citys", JSON.stringify(citys));
+  }
+  if (localStorage.getItem("places") === null) {
+    localStorage.setItem("places", JSON.stringify(places));
+  }
+  if (localStorage.getItem("trips") === null) {
+    localStorage.setItem("trips", JSON.stringify(trips));
+  }
+  if (localStorage.getItem("users") === null) {
+    localStorage.setItem("users", JSON.stringify(users));
+  }
+};
+
+export { initAllData, citys, trips};
