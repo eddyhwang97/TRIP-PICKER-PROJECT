afc32b6ffd9ade6821e110c7b3fd6e745b8f992e
eddyhwang97 <hwangdw1997@gmail.com>
2025-04-24 16:05:02 +0900

    .

--- my-app/src/stores/store.API.jsx
+++ my-app/src/stores/store.API.jsx
@@ -1,5 +1,5 @@
 import { create } from 'zustand';
 
 export const useStore = create((set) => ({
-    user : JSON.parse(localStorage.getItem("users"))[0] || null,
+    user : JSON.parse(sessionStorage.getItem("users")) || null,
 }));
