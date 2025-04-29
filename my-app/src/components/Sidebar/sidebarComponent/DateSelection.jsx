178c15462c6994af548e71da2bbfa42765e34e30
eddyhwang97 <hwangdw1997@gmail.com>
2025-04-29 22:26:45 +0900

    .

--- my-app/src/components/Sidebar/sidebarComponent/DateSelection.jsx
+++ my-app/src/components/Sidebar/sidebarComponent/DateSelection.jsx
@@ -1,22 +1,57 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import Flatpickr from "react-flatpickr";
+import "flatpickr/dist/flatpickr.min.css";
+import { format, set, setDate } from "date-fns";
+import $ from "jquery";
 // 캘린더
-import DatePicker from "react-datepicker";
 
 // scss
-import editTripSidebar from "../../../views/css/editTripSidebar.scss";
-import "react-datepicker/dist/react-datepicker.css";
-
+// import editTripSidebar from "../../../views/css/editTripSidebar.scss";
 export default function DateSelection({ onNext, onPrev }) {
-  return (
+  const [dateRange, setDateRange] = useState([new Date(), new Date()]);
+  
+  const handleDateChange = () => {
+    const setDate = dateRange.map((date)=>date.toISOString().slice(0, 10))
+    console.log(setDate)
+    setTripsDate(setDate)
+
+  }
+  const setTripsDate = (setDate) => {
+    const trips = JSON.parse(localStorage.getItem("trips"))
+    const trip = trips.find((trip) => trip.userId === user.id)
+    const startDate = setDate[0]
+    const edfDate = setDate[1]
     
-    <div className="date-selection">
-      <p>날짜를 선택해주세요.</p>
+  }
+  useEffect(()=>{
+    $('.flatpickr-input').css({display:"none"})
+    handleDateChange()
+  })
+  return (
+    <div className="date-select-container">
+      <div className="p-6">
+        <div>
+          <Flatpickr
+            options={{
+              mode: "range",
+              dateFormat: "Y-m-d",
+              inline: true, // Display calendar inline
+              static: true, // Prevents input field from being added
+            }}
+            value={dateRange}
+            onChange={(dates) => setDateRange(dates)}
+          />
+        </div>
+        <p><strong>Start Date:</strong> {dateRange[0] ? format(dateRange[0], "yyyy-MM-dd") : "Not selected"}</p>
+        <p><strong>End Date:</strong> {dateRange[1] ? format(dateRange[1], "yyyy-MM-dd") : "Not selected"}</p>
+        
+      </div>
       <div className="button-group">
-        <button className="prev-button" onClick={onPrev}>
+      <button className="prev-button" onClick={()=>{onPrev()}  }>
           이전
         </button>
         <button className="next-button" onClick={onNext}>
-          활동 시간 설정하기 
+         활동 시간 선택하기
         </button>
       </div>
     </div>
