5e46c7641bf7abd8e4b9849281f5734ed811b697
Hyeonseok0529 <gustjr8293@naver.com>
2025-06-02 13:09:00 +0900

    메인페이지 미디어 쿼리

--- my-app/src/layouts/HeaderArea.jsx
+++ my-app/src/layouts/HeaderArea.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { useStore } from "../stores/store.API";
 import { Link } from "react-router-dom";
@@ -7,6 +8,7 @@ import smallLogo from "../assets/logo/small-logo.png";
 import "./css/headerArea.scss";
 
 function HeaderArea(props) {
+  const [menuOpen, setMenuOpen] = useState(false);
   const user = useStore((state) => state.user);
   const clearUser = useStore((state) => state.clearUser);
   const navigate = useNavigate();
@@ -28,21 +30,29 @@ function HeaderArea(props) {
             <img src={smallLogo} />
           </Link>
         </section>
-        <section className="nav-box">
-          <Link to="dashboard">dashboard</Link>
-          {user ? (
-            <>
-              <a href="/" onClick={handleLogout}>
-                Logout
-              </a>
-            </>
-          ) : (
-            <>
-              <Link to="login">login</Link>
-              <Link to="join">join</Link>
-            </>
-          )}
-        </section>
+            <section className="nav-box">
+      {/* 햄버거 아이콘 (모바일용) */}
+      <div className="hamburger" onClick={() => setMenuOpen(!menuOpen)}>
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+
+      {/* 메뉴 목록 */}
+      <nav className={`nav-menu ${menuOpen ? "open" : ""}`}>
+        <Link to="dashboard">DASHBOARD</Link>
+        {user ? (
+          <a href="/" onClick={handleLogout}>
+            LOGOUT
+          </a>
+        ) : (
+          <>
+            <Link to="login">LOGIN</Link>
+            <Link to="join">JOIN</Link>
+          </>
+        )}
+      </nav>
+    </section>
       </div>
     </>
   );
