694b108879e1690993493f7c3d8f1bef54603c3a
Hyeonseok0529 <gustjr8293@naver.com>
2025-06-06 15:28:17 +0900

    네비게이션 작업 중

--- my-app/src/layouts/HeaderArea.jsx
+++ my-app/src/layouts/HeaderArea.jsx
@@ -8,7 +8,6 @@ import smallLogo from "../assets/logo/small-logo.png";
 import "./css/headerArea.scss";
 
 function HeaderArea(props) {
-  const [menuOpen, setMenuOpen] = useState(false);
   const navRef = useRef(null);
   const location = useLocation();
   const user = useStore((state) => state.user);
@@ -23,22 +22,6 @@ function HeaderArea(props) {
     }
   };
 
-  // Effect : 바깥 클릭 시 메뉴 닫기 //
-  useEffect(() => {
-    function handleClickOutside(e) {
-      if (navRef.current && !navRef.current.contains(e.target)) {
-        setMenuOpen(false);
-      }
-    }
-    document.addEventListener("mousedown", handleClickOutside);
-    return () => document.removeEventListener("mousedown", handleClickOutside);
-  }, []);
-
-  // Effect : 페이지 이동시 메뉴 닫기 //
-  useEffect(() => {
-    setMenuOpen(false);
-  }, [location]);
-
   return (
     <>
       <div className="header">
@@ -48,19 +31,10 @@ function HeaderArea(props) {
           </Link>
         </section>
         <section className="nav-box" ref={navRef}>
-          {/* 햄버거 아이콘 (모바일용) */}
-          <div
-            className="hamburger"
-            onClick={() => setMenuOpen((prev) => !prev)}
-          >
-            <span></span>
-            <span></span>
-            <span></span>
-          </div>
 
           {/* 네비게이션 메뉴 */}
-          <nav className={`nav-menu ${menuOpen ? "open" : ""}`}>
-            <Link to="/dashboard" onClick={() => setMenuOpen(false)}>
+          <nav className='nav-menu'>
+            <Link to="/dashboard">
               DASHBOARD
             </Link>
             {user ? (
@@ -69,17 +43,16 @@ function HeaderArea(props) {
                 onClick={(e) => {
                   e.preventDefault();
                   handleLogout();
-                  setMenuOpen(false);
                 }}
               >
                 LOGOUT
               </a>
             ) : (
               <>
-                <Link to="/login" onClick={() => setMenuOpen(false)}>
+                <Link to="/login">
                   LOGIN
                 </Link>
-                <Link to="/join" onClick={() => setMenuOpen(false)}>
+                <Link to="/join" >
                   JOIN
                 </Link>
               </>
